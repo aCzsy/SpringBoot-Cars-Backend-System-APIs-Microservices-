46c9fdc21d75393ab739dc099a5fa57c4b585e14
Artis Brizs <artis.brizs@student.griffith.ie>
2021-08-19 18:12:17 +0100

    converted pricing service into microservice, created eureka and registered service

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,23 +1,31 @@
 package com.udacity.pricing.domain.price;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Entity
 public class Price {
 
     private String currency;
     private BigDecimal price;
-    private Long vehicleId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     public Price() {
     }
 
-    public Price(String currency, BigDecimal price, Long vehicleId) {
+    public Price(String currency, BigDecimal price, Long id) {
         this.currency = currency;
         this.price = price;
-        this.vehicleId = vehicleId;
+        this.id = id;
     }
 
     public String getCurrency() {
@@ -37,10 +45,10 @@ public class Price {
     }
 
     public Long getVehicleId() {
-        return vehicleId;
+        return id;
     }
 
     public void setVehicleId(Long vehicleId) {
-        this.vehicleId = vehicleId;
+        this.id = vehicleId;
     }
 }
